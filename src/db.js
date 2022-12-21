9a50606e38cdccc002be4efd92138a3682dbf685
zerontec <zerontecnologia1@gmail.com>
2022-12-21 00:38:30 -0400

    change again data conection

--- src/db.js
+++ src/db.js
@@ -22,13 +22,6 @@ const pool = new Pool({
 
 
 
-client.connect((err) => {
-  if (err) {
-    console.error('connection error', err.stack)
-  } else {
-    console.log('connected')
-  }
-})
 let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({

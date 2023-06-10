645c94b1f5f555853f20faaab0315cf0a4c1afcd
zerontec <zerontecnologia1@gmail.com>
2023-06-10 03:13:16 -0400

    update all

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,8 +3,8 @@ const router = Router();
 const {registerUser, loginUser} = require ('../controller/auth.controller');
 const { checkDuplicateUsernameOrEmail, checkRolesExisted} = require('../middleware/verifyAuth');
 
-router.post('/register-user',[checkDuplicateUsernameOrEmail, checkRolesExisted],registerUser);
-router.post('/login-user', loginUser);
+router.post('/register',[checkDuplicateUsernameOrEmail, checkRolesExisted],registerUser);
+router.post('/login', loginUser);
 
 
 

fb11e83612cda7f254c271a09b3f5c1b128a513f
zerontec <zerontecnologia1@gmail.com>
2023-07-02 18:10:49 -0400

    new controler stats and sales report

--- src/controller/seller.controller.js
+++ src/controller/seller.controller.js
@@ -1,4 +1,4 @@
-const { Seller } = require("../db");
+const { Seller, InvoiceFactura } = require("../db");
 const Sequelize = require("sequelize");
 const { Op } = require("sequelize");
 
@@ -150,7 +150,108 @@ const searchSellerByCode = async (req, res, next) => {
   }
 }
 
-  
+
+
+// Controlador para obtener las estadísticas de ventas de un vendedor
+const getSalesStats = async (req, res, next) => {
+  try {
+    const { sellerId } = req.params;
+
+    // Obtener la fecha actual
+    const currentDate = new Date();
+
+    // Obtener la fecha de inicio y fin del día actual
+    const startOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+    const endOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1);
+
+    // Obtener la fecha de inicio y fin de la semana actual
+    const startOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay());
+    const endOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay() + 7);
+
+    // Obtener la fecha de inicio y fin del mes actual
+    const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+    const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+
+    // Consultar las ventas del vendedor para el día actual
+    const daySales = await InvoiceFactura.sum('amount', {
+      where: {
+        sellerId,
+        date: {
+          $gte: startOfDay,
+          $lt: endOfDay
+        }
+      }
+    });
+
+    // Consultar las ventas del vendedor para la semana actual
+    const weekSales = await InvoiceFactura.sum('amount', {
+     
+      where: {
+        sellerId:sellerId,
+        date: {
+          $gte: startOfWeek,
+          $lt: endOfWeek
+        }
+      }
+    });
+
+    // Consultar las ventas del vendedor para el mes actual
+    const monthSales = await InvoiceFactura.sum('amount', {
+      where: {
+        sellerId:sellerId,
+       sellerId,
+        date: {
+          $gte: startOfMonth,
+          $lt: endOfMonth
+        }
+      }
+    });
+
+    res.json({
+      daySales: daySales || 0,
+      weekSales: weekSales || 0,
+      monthSales: monthSales || 0
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+const getProductsBySeller = async (req, res, next) => {
+  try {
+    const { sellerId } = req.params;
+
+    // Verificar si el vendedor existe
+    const seller = await Seller.findByPk(sellerId);
+    if (!seller) {
+      return res.status(404).json({ message: 'Vendedor no encontrado' });
+    }
+
+    // Obtener los productos vendidos por el vendedor
+    const products = await InvoiceFactura.findAll({
+      where: {
+       sellerId,
+      },
+      attributes: ['productoFactura'],
+      include: [{ model: Seller }],
+    });
+
+    // Crear una lista de los productos vendidos
+    const productList = [];
+    products.forEach((invoice) => {
+      const { productoFactura } = invoice;
+      productoFactura.forEach((product) => {
+        productList.push(product);
+      });
+    });
+
+    res.status(200).json({ seller, products: productList });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener los productos del vendedor', error });
+  next(error)
+  }
+};
 
 
 
@@ -162,5 +263,7 @@ module.exports = {
   findOne,
   editSeller,
   deleteSeller,
-  searchSellerByCode
+  searchSellerByCode,
+  getProductsBySeller,
+  getSalesStats
 };

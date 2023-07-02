ca45127584c81565fc29e8fefa48de8390b06c9b
zerontec <zerontecnologia1@gmail.com>
2023-07-02 18:09:52 -0400

    router seller

--- src/routes/seller.routes.js
+++ src/routes/seller.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
-const { createSeller, findAll, findOne, editSeller, deleteSeller, searchSellerByCode } = require('../controller/seller.controller');
+const { createSeller, findAll, findOne, editSeller, deleteSeller, searchSellerByCode, getSalesStats, getProductsBySeller } = require('../controller/seller.controller');
+const { getProductById } = require('../controller/invetory.controller');
 const router = Router();
 
 
@@ -11,8 +12,8 @@ router.get('/find-one/:id', findOne)
 router.put('/update/:id', editSeller);
 router.delete('/delete/:id', deleteSeller)
 router.get('/search-query', searchSellerByCode)
-
-
+router.get('/seller-stast/:sellerId', getSalesStats);
+router.get('/seller-product-sales/:sellerId', getProductsBySeller)
 
 
 

3a970d6c94c13bc014e494102ebcc85bc7cd4812
zerontec <zerontecnologia1@gmail.com>
2023-07-12 01:40:42 -0400

    add verification in routes B

--- src/routes/accountR.routes.js
+++ src/routes/accountR.routes.js
@@ -1,15 +1,16 @@
 const {Router}= require('express');
 const { createAccountsReceivable, getAllAccountsReceivable, getAccountsReceivable,updateAccountsReceivable, deleteAccountsReceivable } = require('../controller/accountsReceivable.controller');
+const authJwt = require('../middleware/authJwt');
 
 
 const router = Router()
 
 
-router.post('/create-account', createAccountsReceivable);
-router.get('/all-account', getAllAccountsReceivable);
-router.get('/detail-account/:id', getAccountsReceivable )
-router.put('/update-account/:id', updateAccountsReceivable);
-router.delete('/delete-account/:id', deleteAccountsReceivable);
+router.post('/create-account',authJwt.verifyToken, authJwt.isAdmin, createAccountsReceivable);
+router.get('/all-account',authJwt.verifyToken, authJwt.isAdmin, getAllAccountsReceivable);
+router.get('/detail-account/:id',authJwt.verifyToken, authJwt.isAdmin, getAccountsReceivable )
+router.put('/update-account/:id',authJwt.verifyToken, authJwt.isAdmin, updateAccountsReceivable);
+router.delete('/delete-account/:id',authJwt.verifyToken, authJwt.isAdmin, deleteAccountsReceivable);
 
 
 
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,21 +1,22 @@
 const {Router} = require ('express');
 const {buscarClientePorIdentificacion, createCustomer, findAllCustomer, FindCustomertByCedula, searchCustomer, numberCustomer, deleteCustomer, editCustomer, SearchCustomerById } = require('../controller/customer.controller');
+const authJwt = require('../middleware/authJwt');
 
 
 const router = Router()
 
 
-router.post('/create', createCustomer)
-router.get('/find-all', findAllCustomer)
-router.get('/find-id/:id', FindCustomertByCedula);
-router.post('/search', searchCustomer);
-router.get('/number-customer', numberCustomer);
+router.post('/create',authJwt.verifyToken, authJwt.isAdmin, createCustomer)
+router.get('/find-all',authJwt.verifyToken, authJwt.isAdmin, findAllCustomer)
+router.get('/find-id/:id',authJwt.verifyToken, authJwt.isAdmin, FindCustomertByCedula);
+router.post('/search',authJwt.verifyToken, authJwt.isAdmin, searchCustomer);
+router.get('/number-customer',authJwt.verifyToken, authJwt.isAdmin, numberCustomer);
 
 
-router.delete('/delete/:id', deleteCustomer);
-router.put('/update/:id', editCustomer);
+router.delete('/delete/:id',authJwt.verifyToken, authJwt.isAdmin, deleteCustomer);
+router.put('/update/:id', authJwt.verifyToken, authJwt.isAdmin,editCustomer);
 
-router.get('/search-query', buscarClientePorIdentificacion)
-router.get('/search/:id', SearchCustomerById)
+router.get('/search-query',authJwt.verifyToken,authJwt.isAdminOrFacturacion,  buscarClientePorIdentificacion)
+router.get('/search/:id',authJwt.verifyToken, authJwt.isAdmin, SearchCustomerById)
 
 module.exports = router

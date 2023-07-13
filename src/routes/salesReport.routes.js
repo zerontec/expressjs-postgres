4d50253445abbb2727d026e086bf126dd069c176
zerontec <zerontecnologia1@gmail.com>
2023-07-13 04:14:35 -0400

    sales report router

--- src/routes/salesReport.routes.js
+++ src/routes/salesReport.routes.js
@@ -1,13 +1,13 @@
 const {Router}= require('express');
-const { getDailySalesReport } = require('../controller/CierreVenta');
+const { getDailySalesReport, getAllClosure } = require('../controller/CierreVenta');
 
+const authJwt = require('../middleware/authJwt');
 
 const router = Router();
 
 
 router.get('/sales-report', getDailySalesReport)
-
-
+router.get('/get-all', getAllClosure)
 
 
 

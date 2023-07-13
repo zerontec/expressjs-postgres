907c31c474a457de6e46d7f4e220a181f0f24550
zerontec <zerontecnologia1@gmail.com>
2023-07-13 04:15:18 -0400

    cierre venta controller

--- src/controller/CierreVenta.js
+++ src/controller/CierreVenta.js
@@ -1,4 +1,4 @@
-const {InvoiceFactura, Payment} = require('../db')
+const {InvoiceFactura, SalesClosure} = require('../db')
 const{Op}=require('sequelize')
 
 
@@ -8,6 +8,14 @@ const getDailySalesReport = async (req, res, next) => {
       const currentDate = new Date();
       const currentDateString = currentDate.toISOString().split('T')[0];
   
+ // Verificar si ya existe un registro para la fecha actual
+ const existingReport = await SalesClosure.findOne({ where: { date: currentDateString } });
+
+ if (existingReport) {
+   // Si ya existe un reporte para la fecha actual, enviar un mensaje informando que ya existe
+   return res.status(409).json({ message: 'El reporte de cierre de ventas ya existe para la fecha actual' });
+ }
+
       // Obtener todas las facturas emitidas en el día actual
       const invoices = await InvoiceFactura.findAll({
         where: {
@@ -15,11 +23,6 @@ const getDailySalesReport = async (req, res, next) => {
             [Op.gte]: currentDateString,
           },
         },
-        // include: [
-        //   {
-        //     model: Payment,
-        //   },
-        // ],
       });
   
       // Calcular el total de ventas y los totales por método de pago
@@ -28,13 +31,13 @@ const getDailySalesReport = async (req, res, next) => {
   
       invoices.forEach(invoice => {
         // Obtener el monto total de la factura
-        const invoiceTotal = invoice.total;
+        const invoiceTotal = parseFloat(invoice.amount);
   
         // Incrementar el total de ventas
         totalSales += invoiceTotal;
   
         // Obtener los métodos de pago asociados a la factura
-        const payments = InvoiceFactura.metodoPago;
+        const payments = invoice.metodoPago;
   
         payments.forEach(payment => {
           // Obtener el método de pago y el monto
@@ -56,6 +59,15 @@ const getDailySalesReport = async (req, res, next) => {
         totalSales,
         paymentTotals,
       };
+
+ // Guardar el registro del cierre de ventas en la base de datos
+ const salesClosure = new SalesClosure({
+    date: currentDateString,
+    totalSales: totalSales.toFixed(2),
+    paymentTotals: JSON.stringify(paymentTotals), // Convertir a formato JSON antes de guardar en la base de datos
+  });
+
+  await salesClosure.save();
   
       res.status(200).json(salesReport);
     } catch (error) {
@@ -64,12 +76,39 @@ const getDailySalesReport = async (req, res, next) => {
     }
   };
 
-  
 
+
+
+const getAllClosure =async(req, res, next)=> {
+
+try{
+
+    const sales = await SalesClosure.findAll()
+    if(!sales){
+
+        return res.status(400).json({message:'no hay informacion que mostrar'})
+
+    }
+
+    res.status(200).json(sales)
+
+
+}catch(error){
+
+    res.status(500),json(error)
+
+}
+
+
+}
+
+
+  
   module.exports = {
 
 
-    getDailySalesReport
+    getDailySalesReport,
+    getAllClosure
 
 
   }

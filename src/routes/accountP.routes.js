5bb6f6c063b8a2e0e607942666cb4bb857220bd8
zerontec <zerontecnologia1@gmail.com>
2023-07-17 22:17:54 -0400

    add auth header in routyer

--- src/routes/accountP.routes.js
+++ src/routes/accountP.routes.js
@@ -1,18 +1,19 @@
 const {Router}= require('express');
 const { createAccountsPayable, getAllAccountsPayable, getAccountsPayable, updateAccountsPayable, deleteAccountsPayable, createPayment, getAllPagoComprasID } = require('../controller/accountPayable.controller');
+const authJwt = require('../middleware/authJwt');
 
 
 
 const router = Router()
 
 
-router.post('/create-account-payable', createAccountsPayable);
-router.get('/all-account-payable', getAllAccountsPayable);
-router.get('/detail-account-payable/:id', getAccountsPayable)
-router.put('/update-account-payable/:id', updateAccountsPayable);
-router.delete('/delete-account-payable/:id', deleteAccountsPayable);
-router.post('/create-pay', createPayment);
-router.post('/get-all-pay', getAllPagoComprasID);
+router.post('/create-account-payable',authJwt.verifyToken, authJwt.isAdmin, createAccountsPayable);
+router.get('/all-account-payable',authJwt.verifyToken, authJwt.isAdmin, getAllAccountsPayable);
+router.get('/detail-account-payable/:id',authJwt.verifyToken, authJwt.isAdmin, getAccountsPayable)
+router.put('/update-account-payable/:id', authJwt.verifyToken, authJwt.isAdmin,updateAccountsPayable);
+router.delete('/delete-account-payable/:id',authJwt.verifyToken, authJwt.isAdmin, deleteAccountsPayable);
+router.post('/create-pay',authJwt.verifyToken, authJwt.isAdmin, createPayment);
+router.post('/get-all-pay',authJwt.verifyToken, authJwt.isAdmin, getAllPagoComprasID);
 
 
 

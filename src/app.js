17153d9362d649192767903d65d1fb328f12b2e9
zerontec <zerontecnologia1@gmail.com>
2024-04-02 23:26:19 -0400

    add xorigin

--- src/app.js
+++ src/app.js
@@ -34,8 +34,8 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header("Access-Control-Allow-Headers",
-  "x-access-token, Origin, Content-Type, Accept")
+  // res.header("Access-Control-Allow-Headers",
+  // "x-access-token, Origin, Content-Type, Accept")
   next();
 }); 
 

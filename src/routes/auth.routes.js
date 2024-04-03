5056aa266540932e873b61f30d54def4536f4937
zerontec <zerontecnologia1@gmail.com>
2024-04-03 11:24:43 -0400

    tes router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const {registerUser, loginUser} = require ('../controller/auth.controller');
 const { checkDuplicateUsernameOrEmail, checkRolesExisted} = require('../middleware/verifyAuth');
 
 router.post('/register',[checkDuplicateUsernameOrEmail, checkRolesExisted],registerUser);
+
 router.post('/login', loginUser);
 
 
